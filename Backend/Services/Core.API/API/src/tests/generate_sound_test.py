5fe5ab7f8523aee67f9fd758921957344a248fc6
lucaslgu <lucas.trompista.lgu@gmail.com>
2022-05-10 23:35:30 -0300

    refactor: :white_check_mark: Changed assert in integration test
    Changed asserts in integration test, for validate return url format

--- Backend/Services/Core.API/API/src/tests/generate_sound_test.py
+++ Backend/Services/Core.API/API/src/tests/generate_sound_test.py
@@ -1,11 +1,22 @@
+from datetime import datetime
 import json
+
+from re import search
 from ..app import app
 
 def test_generate_sound_endpoint():
+    type_binaural = "calm"
+    type_background = "safe_rock"
+    time = 10
     response = app.test_client().post('/api/core/sound/generate/', data=json.dumps({
-                                        'typeBinaural': 'calm',
-                                        'typeBackground': 'safe_rock',
-                                        'time': 10
+                                        'typeBinaural': type_binaural,
+                                        'typeBackground': type_background,
+                                        'time': time
                                     }), content_type='application/json')
 
     assert response.status_code == 200
+    assert response.json["has_error"] == False
+    assert search(datetime.now().date().isoformat(), response.json["url_sound"])
+    assert search(str(time), response.json["url_sound"])
+    assert search(type_binaural, response.json["url_sound"])
+    assert search(type_background, response.json["url_sound"])
