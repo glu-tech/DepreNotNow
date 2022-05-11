f266b40d2bd11fa3bb95ca2b24232b0fd18bd8f5
lucaslgu <lucas.trompista.lgu@gmail.com>
2022-05-10 23:30:56 -0300

    refactor: :lock: Added method capture json envs
    Added method for capture json that contains aws key and aws secret

--- Backend/Services/Core.API/API/src/services/aws_service.py
+++ Backend/Services/Core.API/API/src/services/aws_service.py
@@ -1,11 +1,14 @@
 import boto3
+import json
+import os
 
 class AwsService():
     def upload(self, filename, name, path_bucket):        
         bucket = "depre-not-now"
+        AWSAccessKeyId, AWSSecretKey = self.get_env_aws_key()
         s3 = boto3.Session(
-            aws_access_key_id="XXXXXXXXXXXXXXXXXXXX",
-            aws_secret_access_key="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            aws_access_key_id=AWSAccessKeyId,
+            aws_secret_access_key=AWSSecretKey,
         ).client("s3")
 
         try:
@@ -16,4 +19,15 @@ class AwsService():
     def make_url(self, name, bucket, path):
         base_url = f"https://{bucket}.s3.amazonaws.com/{path}/{name}"
         return base_url
+
+    def get_env_aws_key(self):
+        filepath = f"{os.path.dirname(os.path.realpath('__file__'))}/.env.json"
+        with open(filepath, "r") as file:
+            content = file.read()
+            file.close()
+
+        decoded_json = json.loads(content)
+
+        return decoded_json["AWSAccessKeyId"], decoded_json["AWSSecretKey"]
+
                 
